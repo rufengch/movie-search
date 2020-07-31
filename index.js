a6f7777d14afa72fb7d58f1e83b23112298e2812
Brody Chen <rufengch@gmail.com>
2020-07-31 07:08:39 +0000

    Add results page template

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/results', async (req, res) => {
 	const result = await axios.get('http://www.omdbapi.com/?apikey=26cc215e&s=california');
 	console.log(result.data.Search[0].Title);
-	res.send('result');
+	res.render('results');
 });
 
 const port = process.env.PORT || 3000;
