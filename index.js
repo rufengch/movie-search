e142be3ec50ad912a8ff5e1bac300c6e3f92a79d
Brody Chen <rufengch@gmail.com>
2020-08-01 06:32:43 +0000

    Add search route

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const axios = require('axios');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+	res.render('search');
+});
+
 app.get('/results', async (req, res) => {
-	const result = await axios.get('http://www.omdbapi.com/?apikey=26cc215e&s=california');
-	console.log(result.data.Search[0].Title);
+	const {search} = req.query;
+	console.log('search:', search);
+	const result = await axios.get(`http://www.omdbapi.com/?apikey=26cc215e&s=${search}`);
+	// console.log(result.data.Search[0].Title);
 	res.render('results', {data: result.data});
 });
 
