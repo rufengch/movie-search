415b40d592d6035ea9ae7a462ff20400497c903c
Brody Chen <rufengch@gmail.com>
2020-08-01 06:19:12 +0000

    Display movie titles

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.get('/results', async (req, res) => {
 	const result = await axios.get('http://www.omdbapi.com/?apikey=26cc215e&s=california');
 	console.log(result.data.Search[0].Title);
-	res.render('results');
+	res.render('results', {data: result.data});
 });
 
 const port = process.env.PORT || 3000;
